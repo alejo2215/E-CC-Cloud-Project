9be5993bcb480e482b525119b3371d9e47809d55
alejo2215 <u3153579@uni.canberra.edu.au>
2017-05-04 16:14:13 +1000

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<div style='text-align:center'><h1>Enterprise & Cloud Computing</h1>";
+echo "<h2>Group Name:<h2>","<h4>Team 4<h4>";
+echo "<h2>Member Names:<h2>";
+echo "<h4>Alejandro Segovia<br><h4>";
+echo "<h4>Julien Barbe<br><h4>";
+echo "<h4>Larry Zadiel<h4>";
+
+?>
